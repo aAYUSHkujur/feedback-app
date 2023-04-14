17e8072ba5df9fcb553af42fafa34d04aeed8eb0
aayushk720@gmail.com <aayushk720@gmail.com>
2023-04-14 13:17:27 +0530

    updated

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -17,7 +17,7 @@ export const FeedbackProvider = ({ children }) => {
 
   // Fetch feedback
   const fetchFeedback = async () => {
-    const response = await fetch(`/feedback?sort=id&_order=desc`);
+    const response = await fetch('https://feedback-app-jasonserver.onrender.com/feedback?_sort=id&_order=desc');
     const data = await response.json();
     setFeedback(data);
     setIsLoading(false);
@@ -25,7 +25,7 @@ export const FeedbackProvider = ({ children }) => {
 
   // Add feedback
   const addFeedback = async (newFeedback) => {
-    const response = await fetch("/feedback", {
+    const response = await fetch('https://feedback-app-jasonserver.onrender.com/feedback', {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -39,7 +39,7 @@ export const FeedbackProvider = ({ children }) => {
   // Delete feedback
   const deleteFeedback = async (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
-      await fetch(`/feedback/${id}`, { method: "DELETE" });
+      await fetch(`https://feedback-app-jasonserver.onrender.com/feedback/${id}`, { method: "DELETE" });
 
       setFeedback(feedback.filter((item) => item.id !== id));
     }
@@ -47,7 +47,7 @@ export const FeedbackProvider = ({ children }) => {
 
   // Update feedback item
   const updateFeedback = async (id, updItem) => {
-    const response = await fetch(`/feedback/${id}`, {
+    const response = await fetch(`https://feedback-app-jasonserver.onrender.com/feedback/${id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
